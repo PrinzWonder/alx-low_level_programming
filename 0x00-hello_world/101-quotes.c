9cfa77fc42832469a115408747f2f45dd1624488
prinzwonder <prinz.wonder@gmail.com>
2022-10-13 16:02:13 -0700

    101-quotes.c

--- 0x00-hello_world/101-quotes.c
+++ 0x00-hello_world/101-quotes.c
@@ -1,11 +1,15 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints the standrad error sandwiched around the  standard outpu/t
- *
- * Return: returns 1
- */
+* main - main function in the task
+*
+*
+*Return: return 1
+*/
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
+Footer
+
